052e8bcd8f330a79a15681fd259abfc97a497d7b
tragdir <msiguenza1337@gmail.com>
2024-01-08 02:51:30 -0500

    tested possible other solutions, other solution was slower

--- 2023/01/src/01.rs
+++ 2023/01/src/01.rs
@@ -1,38 +1,36 @@
-use aho_corasick::AhoCorasick;
+use aho_corasick::{AhoCorasick, Match};
 aoc::parts!(1, 2);
-
-#[allow(dead_code)]
-const DIGITS: [&str; 18] = [
-    "1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven", "8",
-    "eight", "9", "nine",
-];
 
 fn part_1(input: aoc::Input) -> impl ToString {
     let mut total: i32 = 0;
 
     for line in input.lines() {
-        let nums = line
+        let nums: Vec<u8> = line
             .chars()
             .filter(|ch| ch.is_ascii_digit())
             .map(|ch| ch as u8 - b'0')
             .collect::<Vec<_>>();
-        let first = *nums.first().unwrap();
-        let last = *nums.last().unwrap();
-        total += (first as i32) * 10 + last as i32;
+        let first = *nums.first().unwrap() as i32;
+        let last = *nums.last().unwrap() as i32;
+        total += first * 10 + last;
     }
-    total.to_string()
+    total
 }
 
 fn part_2(input: aoc::Input) -> impl ToString {
+    let digits = [
+        "1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven",
+        "8", "eight", "9", "nine",
+    ];
     let mut total: i32 = 0;
 
-    let ac = AhoCorasick::new(DIGITS).unwrap();
+    let ac = AhoCorasick::new(digits).unwrap();
     for line in input.lines() {
-        let matches = ac.find_overlapping_iter(line).collect::<Vec<_>>();
-        let first = matches.first().unwrap().pattern().as_usize() / 2 + 1;
-        let last = matches.last().unwrap().pattern().as_usize() / 2 + 1;
+        let matches: Vec<Match> = ac.find_overlapping_iter(line).collect();
+        let first = matches.first().unwrap().pattern().as_i32() / 2 + 1;
+        let last = matches.last().unwrap().pattern().as_i32() / 2 + 1;
 
-        total += (first as i32) * 10 + last as i32;
+        total += first * 10 + last;
     }
-    total.to_string()
+    total
 }
