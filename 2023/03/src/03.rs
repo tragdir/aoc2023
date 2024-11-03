e85148c71b67bdf91718a85212ff6c90881885ff
tragdir <msiguenza1337@gmail.com>
2024-11-02 21:01:24 -0400

    added day 3

--- 2023/03/src/03.rs
+++ 2023/03/src/03.rs
@@ -1,9 +1,14 @@
+#[allow(unused)]
+use aho_corasick::AhoCorasick;
 aoc::parts!(1);
 
+#[allow(dead_code, unused_variables)]
 fn part_1(input: aoc::Input) -> impl ToString {
     0
 }
 
-// fn part_2(input: aoc::Input) -> impl ToString {
-//     0
-// }
+#[allow(dead_code, unused_variables)]
+fn part_2(input: aoc::Input) -> impl ToString {
+    0
+}
+
